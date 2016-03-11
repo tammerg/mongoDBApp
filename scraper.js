17f8296daf5a67581d1b2b5996f9d117961937f2
Tammer <tammergalal@gmail.com>
2016-03-10 21:22:58 -0500

    able to scrape and add into mongoDB

--- scraper.js
+++ scraper.js
@@ -2,24 +2,58 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var cheerio = require('cheerio');
-var PORT = 3000;
+
+//Database configuration
 var mongojs = require('mongojs');
 var databaseUrl = "scraper";
-var collections = ['scraperData'];
+var collections = ["scrapedData"];
 var db = mongojs(databaseUrl, collections);
-db.on('error', function(err){
+db.on('error', function(err) {
   console.log('Database Error:', err);
 });
 
-app.get('/', function(req, res){
-  res.send('Hello World!');
-});
-app.get('/scraper', function(req, res){
-  console.log("this worked!");
-  //da scrape
-  res.send('scrape complete!');
+
+// Routes
+app.get('/', function(req, res) {
+  res.send("Hello world");
 });
 
-app.listen(PORT, function(){
-  console.log('App running on port %s!', PORT);
+//Get from DB
+app.get('/illegal', function(req, res) {
+  db.scrapedData.find({}, function(err, found) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(found);
+    }
+  });
+});
+
+app.get('/scraper', function(req, res) {
+  request('https://news.ycombinator.com/', function(error, response, html) {
+    var $ = cheerio.load(html);
+    $('.title').each(function(i, element) {
+      var title = $(this).children('a').text();
+      var link = $(this).children('a').attr('href');
+
+      if (title && link) {
+        db.scrapedData.save({
+          title: title,
+          link: link
+        }, function(err, saved) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(saved);
+          }
+        });
+      }
+    });
+  });
+  res.send("Scrape Complete");
+});
+
+
+app.listen(3000, function() {
+  console.log('App running on port 3000!');
 });
